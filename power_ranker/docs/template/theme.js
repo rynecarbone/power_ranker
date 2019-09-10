5804d684b81faf339d1416b018928416156369b4
rynecarbone <ryne.carbone@gmail.com>
2019-09-09 23:37:28 -0400

    updated template files for new data format

--- power_ranker/docs/template/theme.js
+++ power_ranker/docs/template/theme.js
@@ -1,19 +1,19 @@
 $(document).ready(function () {
     $('#power_table').DataTable(
-			    { "paging": false,
-				    "info"  : false,						
-						"searching": false,
-					  columnDefs : [
-					     { targets:[11],orderData:[11,0]}
-				     ],
-						 "fnRowCallback": function(nRow, aData, iDisplayIndex, iDisplayIndexFull){
-								     if( aData[11] == "1"){$('td', nRow).addClass('row-tier1'); } 
-								else if( aData[11] == "2"){$('td', nRow).addClass('row-tier2'); } 
-								else if( aData[11] == "3"){$('td', nRow).addClass('row-tier3'); } 
-								else if( aData[11] == "4"){$('td', nRow).addClass('row-tier4'); } 
-								else if( aData[11] == "5"){$('td', nRow).addClass('row-tier5'); } 
-						}
+			    {"paging": false,
+				 "info"  : false,
+				 "searching": false,
+				 columnDefs : [
+				    {targets:[12], orderData:[12,0]}
+				 ],
+				"fnRowCallback": function(nRow, aData, iDisplayIndex, iDisplayIndexFull){
+				    if( aData[12] == "1"){$('td', nRow).addClass('row-tier1'); }
+					else if( aData[12] == "2"){$('td', nRow).addClass('row-tier2'); }
+					else if( aData[12] == "3"){$('td', nRow).addClass('row-tier3'); }
+					else if( aData[12] == "4"){$('td', nRow).addClass('row-tier4'); }
+					else if( aData[12] == "5"){$('td', nRow).addClass('row-tier5'); }
+				    }
 		    	}
-		);
+	);
 });
 
